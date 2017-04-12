443384a911a49488f014a1a4ebde812f6833c71d
Lorenzo Villani <lorenzo@villani.me>
2017-04-12 18:25:37 +0200

    Version 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,16 @@
 # Change Log
 
-All notable changes to this project are documented in this file.
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to
+[Semantic Versioning](http://semver.org/).
+
+## 1.1.0 - 2017-04-12
+
+### Added
+
+* New `-a`, `--append` option to only append new metadata to an existing `metadata.json` file. Kudos
+  to @wojtek-oledzki for contributing this feature.
 
 
 ## 1.0.0 - 2014-08-17
--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from distutils.core import setup
 
-setup(name="vagrant-metadata",
-      version="1.0.2",
-      description="Vagrant metadata.json generator",
-      url="https://github.com/lvillani/vagrant-metadata/",
-      author="Lorenzo Villani",
-      author_email="lorenzo@villani.me",
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-      ],
-      scripts=["bin/vagrant-metadata"],
-      py_modules=["vagrant_metadata"]
-      )
+setup(
+    name="vagrant-metadata",
+    version="1.1.0",
+    description="Vagrant metadata.json generator",
+    url="https://github.com/lvillani/vagrant-metadata/",
+    author="Lorenzo Villani",
+    author_email="lorenzo@villani.me",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    scripts=["bin/vagrant-metadata"],
+    py_modules=["vagrant_metadata"]
+)
